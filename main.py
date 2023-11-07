edbd86d15fd830ace00239a02ca2f0231e67674f
backtyar <b11703597@gmail.com>
2023-11-08 02:40:35 +0600

    last_commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # складе. Класс Customer представляет клиента и имеет атрибут cart для добавления
 # товаров. Класс Store управляет инвентарем и покупками. Реализуйте функциональность
 # добавления товаров в корзину, расчет общей суммы и обновление инвентаря.
-
+#
 class Product:
 
     def __init__(self, name, price, quantity_stock):
@@ -14,29 +14,60 @@ class Product:
     def __repr__(self):
         return f'{self.name}, {self.price}, {self.quantity_stock}'
 
-banana = Product('banana', 150, 5)
-orange = Product('orange', 200, 10)
-onion = Product('onion', 200, 15)
+
 
 
 class Customer:
-    def __init__(self, client_name):
-        self.client_name = client_name
-        self.cart = []
+    def __init__(self, client_name, quantity):
+        self.__client_name = client_name
+        self.__cart = []
+        self.quantity = quantity
+        self.total = []
 
     def add_cart(self, product):
-        self.cart.append(product)
+        self.__cart.append(product)
 
-anna = Customer('Anna')
-anna.add_cart(banana)
-anna.add_cart(onion)
-anna.add_cart(orange)
 
-class Cart:
-    pass
+    def __str__(self):
+        for i in self.__cart:
+            self.total.append(i.price * self.quantity)
+        return f'{self.total}'
 
-class Story:
-    pass
+
+
+
+class Store:
+    __product_story = {}
+
+    def __init__(self, client):
+        self.client = client
+        self.product_class = Product
+        self.client_customer = []
+
+
+    def add_cart_customer(self, name, quantity):
+        if name in Store.__product_story:
+            customer = Customer(self.client, quantity)
+            customer.add_cart(Store.__product_story[name])
+            self.client_customer.append(customer)
+            print(customer)
+        else:
+            return 'Not product'
+
+    @classmethod
+    def add_product_story(cls, name, price, quantity):
+        product = Product(name, price, quantity)
+        Store.__product_story[product.name] = product
+
+
+
+s = Store('Anna')
+Store.add_product_story('banana', 150, 100)
+Store.add_product_story('orange',  100, 100)
+Store.add_product_story('onion', 150, 100)
+s.add_cart_customer('onion',5)
+s.add_cart_customer('banana',10)
+
 
 
 
@@ -49,63 +80,92 @@ class Story:
 # пользователях и их списки задач. Реализуйте возможность регистрации и
 # авторизации пользователей.
 
-# class Task:
-#
-#     def __init__(self, name, ops, status, level):
-#         self.name = name
-#         self.ops = ops
-#         self.status = status
-#         self.level = level
-#
-# class TaskList:
-#
-#     def __init__(self):
-#         self.task_list = []
-#
-#     def add_task(self, task):
-#         self.task_list.append(task)
-#
-#     def del_task(self, task):
-#         self.task_list.remove(task)
-#
-#     def filter_task(self):
-#         res = sorted(self.task_list)
-#         for i in res:
-#             print(i)
-#
+class Task:
+    def __init__(self, name, ops, status, level=1):
+        self.name = name
+        self.ops = ops
+        self.level = level
+        if status == True:
+            self.status = '✅'
+        else:
+            self.status = '-'
+
+task_1 = Task('Сумма','Целочисленная арифметика',False,5)
+task_2 = Task('Пятью пять - двадцать пять!','Целочисленная арифметика',True,2)
+task_3 = Task('Статистика','Сортировка и последовательности',True,3)
+task_4 = Task('Золото племени АББА','Длинная арифметика',False,8)
+task_5 = Task('Лесенка','Рекурсия, перебор',True,5)
+
+class TaskList:
+    def __init__(self):
+        self.task_list = []
+
+    def add_task(self, task):
+        self.task_list.append(task)
+
+    def del_task(self, task):
+        self.task_list.remove(task)
+
+    def filter_task(self):
+        return sorted(self.task_list, key=lambda x: x.level)
+
+ts_1 = TaskList()
+ts_1.add_task(task_1)
+ts_1.add_task(task_2)
+ts_1.add_task(task_3)
+ts_1.add_task(task_4)
+ts_1.add_task(task_5)
 
 
-#
-# class User:
-#     __password = {}
-#     def __init__(self, task_list):
-#         self.user = ''
-#         self.task_list = task_list
-#
-#     def __registration(self):
-#         self.user = input('Name: ').strip()
-#         password = input('password: ').strip()
-#         User.__password[self.user] = password
-#         self.access_task()
-#
-#
-#
-#     def __authorization(self):
-#         password = input('Введите пороль: ').strip()
-#         if User.__password[self.user] == password:
-#             self.__decide_task()
-#
-#
-#     def __decide_task(self):
-#         print('00000000')
-#         return '0000000000000'
-#
-#
-#     def access_task(self):
-#         if self.user == '':
-#             self.__registration()
-#         else:
-#             self.__authorization()
-#
-# u = User([])
-# u.access_task()
+class User:
+    __password = {}
+    def __init__(self, task_list):
+        self.user = ''
+        self.task_list = task_list
+
+    def __registration(self):
+        print('зополните поля для регистрации')
+        self.user = input('Name: ').strip()
+        password = input('password: ').strip()
+        User.__password[self.user] = password
+        self.access_task()
+
+    def __authorization(self):
+        print(f'Пользователь {self.user}')
+        password = input('Введите пороль: ').strip()
+        if User.__password[self.user] == password:
+            self.__decide_task()
+
+
+    def __decide_task(self):
+        print('Можете приступить к решению задач')
+
+
+
+    def access_task(self):          # пользователь получает доступ к задачам  через функцию access_task
+        if self.user == '':         #
+            self.__registration()   # перенопровляеть __registration
+        else:
+            self.__authorization()  # перенопровляеть __authorization
+
+    def info_users(self):
+        if self.user != '':
+            print(f'Пользователь: {self.user}')
+            for tusk in self.task_list:
+                print(f'Называние:{{{tusk.name}}}, описание: {{{tusk.ops}}},'
+                      f'статус: {{{tusk.status}}}, уровень: {{{tusk.level}}}')
+            else:
+                return ''
+
+
+
+
+
+
+anna = User(ts_1.filter_task())     # добавляем список задач к классу User с помощью ts_1.filter_task() - эгземпляр класса TaskList
+anna.access_task()
+print('*' * 50)
+anna.info_users()
+
+
+
